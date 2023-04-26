40f58c74ec6f6fc955ceffa48840fe300defd979
Patrick <parkpatrick1@gmail.com>
2023-04-26 18:20:28 +0300

    override test class property source

--- src/test/java/leanmanager2/LeanManager2Test.java
+++ src/test/java/leanmanager2/LeanManager2Test.java
@@ -3,6 +3,7 @@ package leanmanager2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +20,8 @@ import org.springframework.web.context.WebApplicationContext;
 import org.hamcrest.Matchers;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = LeanManager2.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class LeanManager2Test {
 
     @Autowired
